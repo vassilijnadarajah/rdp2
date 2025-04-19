599ce90caadcb92795360888f2efcb3957d70d8a
Vassilij Nadarajah <mail@vassilijnadarajah.com>
2025-04-19 19:59:21 +0200

    add tests for point2d functions

--- src/objects.rs
+++ src/objects.rs
@@ -7,8 +7,8 @@ pub struct Point2d {
 }
 
 impl Point2d {
-    pub fn new(x: f64, y: f64) -> Self {
-        Point2d { x, y }
+    pub fn new<T: Into<f64>>(x: T, y: T) -> Self {
+        Point2d { x: x.into(), y: y.into() }
     }
 
     pub fn abs(&self) -> f64 {
@@ -63,5 +63,31 @@ impl Mul<f64> for Point2d {
             x: self.x * scalar,
             y: self.y * scalar,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calc_abs_of_point2d_vector() {
+        let point_1: Point2d = Point2d::new(3,4);
+        let point_2: Point2d = Point2d::new(0,0);
+
+        let abs_1 = point_1.abs();
+        let abs_2 = point_2.abs();
+
+        assert_eq!(abs_1, 5.0);
+        assert_eq!(abs_2, 0.0);
+    }
+
+    #[test]
+    fn calc_norm_of_point2d_vector() {
+        let point_1: Point2d = Point2d::new(3,4);
+
+        let norm_1 = point_1.norm();
+
+        assert_eq!(norm_1, Point2d::new(0.6, 0.8));
     }
 }
